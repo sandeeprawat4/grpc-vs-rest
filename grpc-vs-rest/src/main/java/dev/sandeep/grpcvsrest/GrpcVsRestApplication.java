5b69ac2ed2f543cbf8bed1119dffac9fa4a5347f
sandeep.rawat4 <sandeep.rawat4@gmail.com>
2023-09-20 11:55:43 +0530

    First Commit

---
+++ grpc-vs-rest/src/main/java/dev/sandeep/grpcvsrest/GrpcVsRestApplication.java
@@ -0,0 +1,13 @@
+package dev.sandeep.grpcvsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcVsRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcVsRestApplication.class, args);
+	}
+
+}
---
+++ grpc-vs-rest/src/test/java/dev/sandeep/grpcvsrest/GrpcVsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sandeep.grpcvsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcVsRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
